ea0b2af0498007a84ca8aa93e11ba632cc820936
afifym <mohamed.afify323@gmail.com>
2021-09-28 13:02:51 +0200

    Add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import ReactGA from 'react-ga';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    console.log('analytics ---------------------');
+
+    ReactGA.initialize(`${process.env.ANALYTICS_ID}`);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
