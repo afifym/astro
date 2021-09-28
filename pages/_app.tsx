99c39eea579d9fb293e5cd03ee1c6f4b38565d76
afifym <mohamed.afify323@gmail.com>
2021-09-28 13:07:57 +0200

    Add analytics 2nd method

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,39 @@ import { useEffect } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import ReactGA from 'react-ga';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    console.log('analytics ---------------------');
+  // useEffect(() => {
+  //   console.log('analytics ---------------------');
 
-    ReactGA.initialize(`${process.env.ANALYTICS_ID}`);
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+  //   ReactGA.initialize(`${process.env.ANALYTICS_ID}`);
+  //   ReactGA.pageview(window.location.pathname + window.location.search);
+  // }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.ANALYTICS_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.ANALYTICS_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+          }}
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
